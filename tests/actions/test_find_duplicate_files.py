431a37d443dff2c1025e4335643c7b9f6a274760
Marco Viana <marcoviana100@gmail.com>
2023-08-25 01:51:33 -0300

    refactor: retirando linhas não necessárias

--- tests/actions/test_find_duplicate_files.py
+++ tests/actions/test_find_duplicate_files.py
@@ -6,15 +6,12 @@ from pro_filer.actions.main_actions import find_duplicate_files  # NOQA
 @pytest.fixture
 def context_with_files(tmp_path):
     arq_1 = tmp_path / "arq_1.txt"
-    arq_1.write_text("test content" * 8)
+    arq_1.write_text("testando" * 8)
 
     aqr_2 = tmp_path / "aqr_2.txt"
-    aqr_2.write_text("test content" * 8)
+    aqr_2.write_text("testando" * 8)
 
-    arq_3 = tmp_path / "arq_3.txt"
-    arq_3.write_text("corinthians")
-
-    return {"all_files": [str(arq_1), str(aqr_2), str(arq_3)]}
+    return {"all_files": [str(arq_1), str(aqr_2)]}
 
 
 def test_find_duplicate_files_empty_list(context_with_files):
