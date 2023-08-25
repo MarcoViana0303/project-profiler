133f6af5ff86697c778659b47b0e0eef648b1ac8
Marco Viana <marcoviana100@gmail.com>
2023-08-25 03:11:01 -0300

    chore: adicionando anotações no arquivo de testes para def show_details

--- tests/actions/test_show_details.py
+++ tests/actions/test_show_details.py
@@ -4,20 +4,24 @@ from unittest.mock import patch
 from pro_filer.actions.main_actions import show_details
 
 
+# Fixture para fornecer contexto de arquivo existente
 @pytest.fixture
 def context_existing_file():
     return {"base_path": "path/to/existing_file.txt"}
 
 
+# Fixture para fornecer contexto de arquivo não existente
 @pytest.fixture
 def context_non_existing_file():
     return {"base_path": "path/to/non_existing_file.txt"}
 
 
+# Testa a função show_details com um arquivo existente
 def test_show_details_existing_file(capsys, context_existing_file):
-    mtime = 1678838400  # Timestamp for a specific date
+    mtime = 1678838400  # Timestamp para uma data específica
     expected_mod_date = date.fromtimestamp(mtime)
 
+    # Mocks para simular as funções de sistema operacional
     with patch("os.path.exists", return_value=True):
         with patch("os.path.getsize", return_value=1234):
             with patch("os.path.isdir", return_value=False):
@@ -35,6 +39,7 @@ def test_show_details_existing_file(capsys, context_existing_file):
                         assert captured.out == expected_output
 
 
+# Testa a função show_details com um arquivo não existente
 def test_show_details_non_existing_file(capsys, context_non_existing_file):
     with patch("os.path.exists", return_value=False):
         expected_output = "File 'non_existing_file.txt' does not exist\n"
@@ -43,10 +48,12 @@ def test_show_details_non_existing_file(capsys, context_non_existing_file):
         assert captured.out == expected_output
 
 
+# Testa a função show_details com um arquivo cuja extensão é None
 def test_show_details_file_extension_none(capsys, tmp_path):
     file_content = ""
     file_path = tmp_path / "out_file"
 
+    # Cria um arquivo vazio no diretório temporário
     with open(file_path, "w") as file:
         file.write(file_content)
 
